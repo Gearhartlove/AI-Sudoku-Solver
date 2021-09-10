3e17706ad7a5dc467280d0f4a2107ac30cda2638
Gearhartlove <finley@kristoff.codes>
2021-09-09 18:43:06 -0600

    Added Hello World to Program.cs

--- AI-Sudoku-Solver/Program.cs
+++ AI-Sudoku-Solver/Program.cs
@@ -1,5 +1,11 @@
-﻿namespace AI_Sudoku_Solver {
+﻿using System;
+
+namespace AI_Sudoku_Solver {
 	internal class Program {
-		public static void Main(string[] args) { }
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!!!");
+			Console.WriteLine("Hello Again!");
+		}
 	}
 }
