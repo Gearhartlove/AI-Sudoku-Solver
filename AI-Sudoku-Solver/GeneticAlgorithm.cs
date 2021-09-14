e0078955806b6515bb65f76122638f240930fabf
Kristoff <finley@kristoff.codes>
2021-09-14 13:08:19 -0600

    Random Population Generation

--- AI-Sudoku-Solver/GeneticAlgorithm.cs
+++ AI-Sudoku-Solver/GeneticAlgorithm.cs
@@ -1,24 +1,59 @@
+using System;
+using System.Data.Common;
+
 namespace AI_Sudoku_Solver
 {
     public class GeneticAlgorithm
     {
        //Generate Completely Random Board for Population
-       private int Temperature = 0;
-       private const int pop_size = 20;
-       private SudokuPuzzle[] Population;
+       private const int popSize = 3;
+       private Random rand;
+       private SudokuPuzzle OriginalPuzzle;
+       
        
        //tournament selection
        //rinse and repeat until I find an ideal board
 
        public GeneticAlgorithm()
        {
-           Population = new SudokuPuzzle[pop_size];
+           //array instantiation
+           rand = new Random();
+           OriginalPuzzle = new SudokuPuzzle("Easy-P1.csv"); //TODO Change later for all files implementation
+           SudokuPuzzle[] blankPopulation = new SudokuPuzzle[popSize];
+           SudokuPuzzle[] randomPopulation = GenerateRandomizePopulation(blankPopulation, in OriginalPuzzle);
            //GenerateRandomPop(); //can be used by both Local Search Algorithms
            //   -> work with the SudokuPuzzle Class
            //HillClimb(); //can be used by both Local Search Algorithms //Annealing will have a more tricks
        }
 
-       
+       /// <summary>
+       /// Randomizes the initial population
+       /// </summary>
+       private SudokuPuzzle[] GenerateRandomizePopulation(SudokuPuzzle[] population, in SudokuPuzzle original)
+       {
+           for (int i = 0; i < popSize; i++)
+           {
+               SudokuPuzzle newRandPop = original; //Q: how to assign this variable to a value???
+               
+               //go through each cell, check if it can be change; if it can be changed, assign random number to it]
+               for (int column = 0; column < 9; column++)
+               {
+                   for (int row = 0; row < 9; row++)
+                   {
+                       //check if cell is changeable
+                       //set value:
+                       newRandPop.setValue(column, row, rand.Next(0, 10));
+                   }
+               }
+              
+               Console.WriteLine("Random Generated: ");
+               Console.WriteLine(newRandPop);
+               //randomize locations
+               
+               population[i] = newRandPop;
+           }
+           return population;
+       }
        
        /// <summary>
        /// Mutates Population by using the X operation and Y Selection. (Still need to decide on these)
--- AI-Sudoku-Solver/Program.cs
+++ AI-Sudoku-Solver/Program.cs
@@ -2,9 +2,10 @@
 
 namespace AI_Sudoku_Solver {
 	internal class Program {
-		public static void Main(string[] args) {
-			var t = new SudokuPuzzle("Easy-P1.csv");
-			Console.Out.WriteLine(t.isLegalSquare(0, 0, 1));
+		public static void Main(string[] args)
+		{
+			GeneticAlgorithm ga = new GeneticAlgorithm();
+			
 		}
 	}
 }
