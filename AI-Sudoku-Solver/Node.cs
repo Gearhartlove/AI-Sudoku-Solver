fd868132686254f5d42979c66714157e1e76302f
Gearhartlove <finley@kristoff.codes>
2021-09-14 08:12:51 -0600

    Revert "Revert "Merge pull request #1 from Gearhartlove/node-branch""
    This reverts commit ad6961ce9980082f028d9dadf2ed404f5ae5d8e7.

---
+++ AI-Sudoku-Solver/Node.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+namespace AI_Sudoku_Solver
+{
+    public class Node
+    {
+        public Node parent;
+        public List<Node> next_nodes = new List<Node>();
+        private int value = 0; //Sudoku Value
+    }
+    
+    //Delve()
+    
+    //Backtrack() ? 
+}
