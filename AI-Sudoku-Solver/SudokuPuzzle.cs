24b02f827da36dd3adb4394b171eec4986c69626
Brooks Palin <brookspalin710@gmail.com>
2021-09-16 17:58:28 -0600

    constraint test working as intended fixed gitignore

--- AI-Sudoku-Solver/SudokuPuzzle.cs
+++ AI-Sudoku-Solver/SudokuPuzzle.cs
@@ -5,25 +5,51 @@ public class SudokuPuzzle {
 
 	private int[,] cells = new int[9,9];
 	private bool[,] lockedCells = new bool[9,9];
+	private int spacesLeft = 81;
 
+	public SudokuPuzzle() {
+		for(int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				cells[i, j] = -1;
+				lockedCells[i, j] = false;
+			}
+		}
+	}
+	
+	public SudokuPuzzle(int[,] cells, bool[,] lockedCells, int spacesLeft) {
+		this.cells = cells;
+		this.lockedCells = lockedCells;
+		this.spacesLeft = spacesLeft;
+	}
+	
 	public SudokuPuzzle(string filename) {
-		fromFile(filename);	
-		
 		for(int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				lockedCells[i, j] = false;
 			}
 		}
+		
+		fromFile(filename);
 	}
 
 	public int setValue(int x, int y, int value) {
+		x = Util.Clamp(x, 0, 8);
+		y = Util.Clamp(y, 0, 8);
+		value = Util.Clamp(value, 1, 9);
+		
 		if (isLocked(x, y)) return cells[x, y];
+		if (cells[x, y] == -1 || cells[x, y] == 0) spacesLeft--;
 		cells[x, y] = value;
 		return value;
 	}
 
 	public int setLockedValue(int x, int y, int value) {
+		x = Util.Clamp(x, 0, 8);
+		y = Util.Clamp(y, 0, 8);
+		value = Util.Clamp(value, 1, 9);
+		
 		if (isLocked(x, y)) return cells[x, y];
+		if (cells[x, y] == -1 || cells[x, y] == 0) spacesLeft--;
 		cells[x, y] = value;
 		lockedCells[x, y] = true;
 		return value;
@@ -32,12 +58,6 @@ public class SudokuPuzzle {
 	public int getValue(int x, int y) {
 		return cells[x, y];
 	}
-
-	public bool getLockedValue(int x, int y)
-	{
-		return lockedCells[x, y];
-	}
-	
 
 	public int[] getRow(int row) {
 		row = Util.Clamp(row, 0, 8);
@@ -67,11 +87,16 @@ public class SudokuPuzzle {
 		
 		for (int j = y * 3; j < (y * 3) + 3; j++) {
 			for (int i = x * 3; i < (x * 3) + 3; i++) {
-				s[i + j * 3] = cells[i, j];
+				s[(i - (x*3))  + ((j - (y*3)) * 3)] = cells[i, j];
 			}
 		}
 
 		return s;
+	}
+	
+	
+	public int[] getSquareXY(int x, int y) {
+		return getSquare((int) Math.Floor((decimal) (x / 3)), (int) Math.Floor((decimal) (y / 3)));
 	}
 
 	public bool isLocked(int x, int y) {
@@ -86,7 +111,7 @@ public class SudokuPuzzle {
 
 		return true;
 	}
-	
+
 	public bool isLegalCol(int col, int value) {
 		col = Util.Clamp(col, 0, 8);
 		for (int i = 0; i < 9; i++) {
@@ -102,11 +127,94 @@ public class SudokuPuzzle {
 
 		for (int j = y * 3; j < (y * 3) + 3; j++) {
 			for (int i = x * 3; i < (x * 3) + 3; i++) {
-				if (cells[i, j] == value) return false;
+				if  (cells[i, j] == value) return false;
 			}
 		}
 
 		return true;
+	}
+	
+	public bool isLegalSquareXY(int x, int y, int value) {
+		return isLegalSquare((int) Math.Floor((decimal) (x / 3)), (int) Math.Floor((decimal) (y / 3)), value);
+	}
+
+	public bool isLegalMove(int x, int y, int value) {
+		return isLegalCol(x, value)
+		    && isLegalRow(y, value)
+		    && isLegalSquare(x / 3, y / 3, value);
+	}
+
+	public int constraintTestRow(int row) {
+		var array = this.getRow(row);
+		var count = 0;
+		
+		for (int i = 0; i < 9; i++) {
+			var value = array[i];
+			if(value == -1) continue;
+			for (int j = 0; j < 9; j++) {
+				if(j == i) continue;
+				if (value == array[j]) {
+					count++;
+					break;
+				}
+			}
+		}
+
+		return count;
+	}
+	
+	public int constraintTestCol(int col) {
+		var array = this.getCol(col);
+		var count = 0;
+		
+		for (int i = 0; i < 9; i++) {
+			var value = array[i];
+			if(value == -1) continue;
+			for (int j = 0; j < 9; j++) {
+				if(j == i) continue;
+				if (value == array[j]) {
+					count++;
+					break;
+				}
+			}
+		}
+
+		return count;
+	}
+	
+	public int constraintTestSquare(int x, int y) {
+		var array = this.getSquare(x, y);
+		var count = 0;
+		
+		for (int i = 0; i < 9; i++) {
+			var value = array[i];
+			if(value == -1) continue;
+			for (int j = 0; j < 9; j++) {
+				if(j == i) continue;
+				if (value == array[j]) {
+					count++;
+					break;
+				}
+			}
+		}
+
+		return count;
+	}
+	
+	public int constraintTestSquareXY(int x, int y) {
+		return constraintTestSquare((int) Math.Floor((decimal) (x / 3)), (int) Math.Floor((decimal) (y / 3)));
+	}
+
+	public int getEmptyCellCount() {
+		return spacesLeft;
+	}
+
+	public int getFilledCellCount() {
+		return 81 - spacesLeft;
+	}
+
+	public SudokuPuzzle copy() {
+		return new SudokuPuzzle(cells.Clone() as int[,], lockedCells.Clone() as bool[,], spacesLeft);
 	}
 
 	private void fromFile(string filename) {
@@ -119,7 +227,7 @@ public class SudokuPuzzle {
 				if (i == "?") {
 					cells[x, y] = -1;
 				} else {
-					this.setLockedValue(x, y, Int32.Parse(i));
+					setLockedValue(x, y, Int32.Parse(i));
 				}
 				x++;
 			}
@@ -155,10 +263,10 @@ public class SudokuPuzzle {
 		string o = "";
 		
 		for (int y = 0; y < 9; y++) {
-			if (y == 0 || y == 3 || y == 6) o += "-------------------\n";
+			if (y == 0 || y == 3 || y == 6) o += "+-----------------------+\n";
 			for (int x = 0; x < 9; x++) {
 				if (x == 0 || x == 3 || x == 6) o += "|";
-				else o += " ";
+				else o += "  ";
 				if (cells[x, y] < 0) o += "?";
 				else o += cells[x, y];
 				if (x == 8) o += "|";
@@ -167,6 +275,8 @@ public class SudokuPuzzle {
 			o += "\n";
 		}
 
+		o += "+-----------------------+";
+		
 		return o;
 	}
 }
