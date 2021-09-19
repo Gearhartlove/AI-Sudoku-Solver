fbadde5b627a86a9549fdb1a727b0f744968d680
Gearhartlove <finley@kristoff.codes>
2021-09-19 10:17:52 -0600

    GA Output (not done yet but close)

--- AI-Sudoku-Solver/Population.cs
+++ AI-Sudoku-Solver/Population.cs
@@ -8,7 +8,6 @@ namespace AI_Sudoku_Solver
         }
         
         public SudokuPuzzle pop;
-        public double tourny_percentage = 0f;
         public int constraints_violated = 0;
     }
 }
